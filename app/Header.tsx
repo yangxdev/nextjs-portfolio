551bb9e4e3f67e489df2323523f3f75c2b544177
yangxdev <j.xiang@campus.unimib.it>
2023-11-11 19:09:05 +0100

    completed: homepage / todo: second page

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,24 +1,23 @@
+"use client";
 import React from 'react';
 import Button from './Button';
 
-type ButtonType = 'header' | 'begin' | 'logo';
-
 export default function Header() {
-    const menuButtons: { type: "header" | "begin" | "logo"; content: string }[] = [
-        { type: 'header', content: 'LinkedIn' },
-        { type: 'header', content: 'GitHub' },
-        { type: 'header', content: 'Resume' },
+    const menuButtons: { type: 'header' | 'begin' | 'logo'; content: string; onClick?: () => void }[] = [
+        { type: 'header', content: 'LinkedIn', onClick: () => window.open('https://www.linkedin.com/in/yangxng/', '_blank') },
+        { type: 'header', content: 'GitHub', onClick: () => window.open('https://github.com/yangxdev', '_blank') },
+        { type: 'header', content: 'Resume', onClick: () => window.open('https://nextjs-enl5l81qm-yangxdev.vercel.app/')},
     ];
 
     return (
-        <div className="header w-full mb-auto py-5 px-6">
+        <div className="header absolute w-full top-0 py-5 px-6">
             <div className="row flex flex-nowrap justify-between items-center">
-                <Button type="logo" content="YANGXDEV" />
+                <Button type="logo" content="YANGXDEV" onClick={() => window.location.reload()} />
                 <div className="menu flex flex-nowrap">
                     {menuButtons.map((button, index) => (
                         <React.Fragment key={index}>
                             {index > 0 && <span className="divider mx-1 opacity-50 select-none">/</span>}
-                            <Button type={button.type} content={button.content} />
+                            <Button type={button.type} content={button.content} onClick={button.onClick} />
                         </React.Fragment>
                     ))}
                 </div>
