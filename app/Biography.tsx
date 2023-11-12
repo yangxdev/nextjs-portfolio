6d8a24cfaddcf902d72ee6bc9f0f193d07ca0f9f
yangxdev <j.xiang@campus.unimib.it>
2023-11-12 14:40:18 +0100

    alignment

---
+++ app/Biography.tsx
@@ -0,0 +1,18 @@
+export default function Biography() {
+    return (
+        <div className="biography mx-40 pt-40">
+        <div className="biography-text">
+            <div className="biography-text-col1 text-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+            explicabo, officiis provident vel voluptas labore illum molestias,
+            hic, dolorum sed aliquid. Perferendis, eaque eos tempora dolor
+            provident nulla corrupti non. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Ut quam dolor ipsum impedit tempore ea amet cum non
+            tempora aut a ad, dignissimos omnis in itaque quae. Blanditiis,
+            doloremque nesciunt?
+            </div>
+            <div className="biography-text-col2"></div>
+        </div>
+        </div>
+    );
+}
--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,67 +1,24 @@
+"use client";
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 import Header from "./Header";
+import Landing from "./Landing";
+import Biography from "./Biography";
 
 export default function HomePage() {
-  const technologies = ["Javascript", "Typescript", "React", "Next.js", "Tailwind CSS", "Java"];
-
   return (
-    <div className="bg-background flex main-page h-screen w-screen items-center justify-center">
-      <Header />
-      <div className="main-content mx-40 select-none py-40 border-b border-slate-700">
-        <div className="landing">
-          <div className="landing-text">
-            <div className="landing-text-primary text-3xl">
-                Hi, I&apos;m <span className="text-primary font-bold">Yang</span>, a
-              Full-Stack Developer with a passion for crafting exceptional
-              front-end experiences.
-            </div>
-            <div className="landing-text-secondary mt-4 text-md font-normal">
-                Currently based in Milan, Italy, I&apos;m a Consultant at <span className="text-accent font-semibold">
-                  Deloitte
-                                Digital
-                </span>, where I bring innovative solutions to life through code
-              and creativity.
-            </div>
-            <div className="landing-text-tertiary mt-4 text-md">
-              My journey involves:
-              <ul className="list-disc pl-6">
-                <li>Designing and implementing user-friendly interfaces.</li>
-                <li>
-                  Collaborating with cross-functional teams to deliver
-                  high-quality software solutions.
-                </li>
-                <li>
-                  Staying updated with emerging web technologies and trends.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="landing-buttons flex flex-nowrap mt-8">
-            <div className="landing-button-github mr-4 w-fit py-2 px-4 font-light border border-slate-500 cursor-pointer 
-              hover:bg-accent hover:text-background ease-in-out duration-200">
-              <a draggable="false" href="https://github.com/yangxdev" target="_blank" rel="noopener noreferrer">
-                Latest work on GitHub
-              </a>
-            </div>
-            <div className="landing-button-email w-fit py-2 px-4 font-light border border-slate-500 cursor-pointer hover:bg-accent hover:text-background ease-in-out duration-200">
-              <a draggable="false" href="mailto:yangxdev@gmail.com" target="_blank" rel="noopener noreferrer">
-                Email Me
-              </a>
-            </div>
-          </div>
-          <div className="landing-subtitle mt-32 text-sm opacity-70 font-light ">
-            {technologies.map((tech, index) => (
-              <React.Fragment key={tech}>
-                {index > 0 && (
-                  <span className="divider mx-2 opacity-50">/</span>
-                )}
-                {tech}
-              </React.Fragment>
-            ))}
-          </div>
-        </div>
+    <div className="bg-background flex-col flex main-page h-screen w-screen items-center justify-center">
+      <div className="scroll-links absolute cursor-pointer">
+        <ScrollLink to="landing" smooth={true} duration={500}>
+          Landing
+        </ScrollLink>
+        <ScrollLink to="biography" smooth={true} duration={500}>
+          Biography
+        </ScrollLink>
       </div>
-      
+      <Header />
+      <Landing />
+      <Biography />
     </div>
   );
 }
