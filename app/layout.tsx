b606484a9cf0b862579922f9e862cd0f578116a7
yangxdev <yangxdev@gmail.com>
2023-12-09 10:41:18 +0100

    Vercel: Speed Insights and Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SpeedInsights />
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
