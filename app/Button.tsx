ac8e10408e5ec3b39ad2c067b49f473c02e0524a
yangxdev <yangxdev@gmail.com>
2023-12-16 12:55:33 +0100

    package.json fix revert 2

--- app/Button.tsx
+++ app/Button.tsx
@@ -2,43 +2,43 @@ import React from "react";
 import styles from "./css/styles.module.css";
 
 type ButtonProps = {
-    type: "begin" | "header" | "logo" | "main" | "generate" | "copy";
-    content: string;
-    disabled?: boolean;
-    onClick?: () => void;
+  type: "begin" | "header" | "logo" | "main" | "generate" | "copy";
+  content: string;
+  disabled?: boolean;
+  onClick?: () => void;
 };
 
 const Button: React.FC<ButtonProps> = ({ type, content, disabled, onClick }) => {
-    let buttonStyle = "";
+  let buttonStyle = "";
 
-    switch (type) {
-        case "begin":
-            buttonStyle = styles["begin-button"];
-            break;
-        case "header":
-            buttonStyle = styles["header-button"];
-            break;
-        case "logo":
-            buttonStyle = styles["logo-button"];
-            break;
-        case "main":
-            buttonStyle = styles["main-button"];
-            break;
-        case "generate":
-            buttonStyle = styles["send-button"];
-            break;
-        case "copy":
-            buttonStyle = styles["copy-button"];
-            break;
-        default:
-            break;
-    }
+  switch (type) {
+    case "begin":
+      buttonStyle = styles["begin-button"];
+      break;
+    case "header":
+      buttonStyle = styles["header-button"];
+      break;
+    case "logo":
+      buttonStyle = styles["logo-button"];
+      break;
+    case "main":
+      buttonStyle = styles["main-button"];
+      break;
+    case "generate":
+      buttonStyle = styles["send-button"];
+      break;
+    case "copy":
+      buttonStyle = styles["copy-button"];
+      break;
+    default:
+      break;
+  }
 
-    return (
-        <div className={`common-button-style transition-all whitespace-nowrap ${buttonStyle} ${disabled ? 'opacity-50 select-none hover:none cursor-default pointer-events-none' : 'cursor-pointer'}`} onClick={disabled ? undefined : onClick}>
-            {content}
-        </div>
-    );
+  return (
+    <div className={`common-button-style transition-all whitespace-nowrap ${buttonStyle} ${disabled ? 'opacity-50 select-none hover:none cursor-default pointer-events-none' : 'cursor-pointer'}`} onClick={disabled ? undefined : onClick}>
+      {content}
+    </div>
+  );
 };
 
 export default Button;
