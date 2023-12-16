510614343726aef41005f0ea73b6aaea927c8612
yangxdev <yangxdev@gmail.com>
2023-12-16 22:04:14 +0100

    Fixed Jest and build issues

---
+++ __tests__/index.test.js
@@ -0,0 +1,104 @@
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Biography from '../app/Biography'
+import Button from '../app/Button'
+import Footer from '../app/Footer'
+import { useInView } from 'react-intersection-observer'
+import { useIsMobile } from '../app/useIsMobile'
+
+jest.mock('react-intersection-observer');
+
+describe('Biography', () => {
+    useInView.mockImplementation(() => [null, true]);
+    test('renders the component', () => {
+        render(<Biography />)
+        const sectionElement = screen.getByTestId('Biography')
+        expect(sectionElement).toBeInTheDocument()
+    })
+
+    test('renders academic journey information', () => {
+        render(<Biography />);
+        const academicJourneyElement = screen.getByText('Academic Journey');
+        expect(academicJourneyElement).toBeInTheDocument();
+    });
+
+    test('renders current job information', () => {
+        render(<Biography />);
+        const currentJobElement = screen.getByText('Current Job');
+        expect(currentJobElement).toBeInTheDocument();
+    });
+
+    test('renders learning journey information', () => {
+        render(<Biography />);
+        const learningJourneyElement = screen.getByText('Learning Journey');
+        expect(learningJourneyElement).toBeInTheDocument();
+    });
+
+    test('renders Soft Skills Development information', () => {
+        render(<Biography />);
+        const softSkillsDevelopmentElement = screen.getByText('Soft Skills Development');
+        expect(softSkillsDevelopmentElement).toBeInTheDocument();
+    });
+
+    test('renders Collaborative Environment information', () => {
+        render(<Biography />);
+        const collaborativeEnvironmentElement = screen.getByText('Collaborative Environment');
+        expect(collaborativeEnvironmentElement).toBeInTheDocument();
+    });
+});
+
+describe('Button', () => {
+    test('renders the button with the correct content', () => {
+        render(<Button type="main" content="Click me" />)
+        const buttonElement = screen.getByText('Click me')
+        expect(buttonElement).toBeInTheDocument()
+    })
+
+    test('applies the correct style based on the button type', () => {
+        render(<Button type="main" content="Click me" />);
+        const buttonElement = screen.getByText('Click me');
+
+        expect(buttonElement).toHaveClass('common-button-style');
+        expect(buttonElement).toHaveClass('main-button');
+    });
+
+    test('handles onClick event when not disabled', () => {
+        const onClickMock = jest.fn();
+        render(<Button type="main" content="Click me" onClick={onClickMock} />);
+        const buttonElement = screen.getByText('Click me');
+
+        fireEvent.click(buttonElement);
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+    });
+
+    test('applies correct styles when disabled', () => {
+        render(<Button type="main" content="Click me" disabled />);
+        const buttonElement = screen.getByText('Click me');
+
+        expect(buttonElement).toHaveClass('opacity-50');
+        expect(buttonElement).toHaveClass('select-none');
+        expect(buttonElement).toHaveClass('cursor-default');
+        expect(buttonElement).not.toHaveClass('cursor-pointer');
+    });
+});
+
+jest.mock('react-responsive', () => ({
+    useMediaQuery: jest.fn(),
+}));
+
+jest.mock('../app/useIsMobile')
+
+describe('Footer', () => {
+    beforeEach(() => {
+        // Reset mocks and set default behavior
+        jest.clearAllMocks();
+        jest.resetModules();
+    });
+
+    test('renders the Footer component', () => {
+        render(<Footer />);
+        const footerElement = screen.getByText("LET'S GET IN TOUCH");
+        expect(footerElement).toBeInTheDocument();
+    });
+});
+
---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+// jest.config.js
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+    // Add more setup options before each test is run
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    // Other Jest configurations...
+
+    // You may need to add moduleNameMapper if you're using CSS or other file types
+    // moduleNameMapper: {
+    //   '\\.(css|less|scss)$': '<rootDir>/path/to/identity-obj-proxy.js',
+    // },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
